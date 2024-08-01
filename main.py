208c4e776425967518460e6e84c2af8ef0649623
PavelKislyaev <pavel.kislyayev@mail.ru>
2024-08-01 20:20:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# - "1st program"
+print(9**0.5*5)
+
+# - "2nd program"
+print(9.99 > 9.98 and 1000!=1000.1)
+
+# - "3rd program
+print(23+67)
+
+# - "4th program"
+print (int(13.42//1)  ==  int(round(42.13 % 1,2)*100))
